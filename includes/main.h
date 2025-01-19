e34533e96d71a56a7c9994e2d59cf56669cff3a9
gnxrlyqf <gnarlydestructor@gmail.com>
2025-01-19 19:45:01 +0100

    update

--- includes/main.h
+++ includes/main.h
@@ -6,6 +6,7 @@
 #include <stdlib.h>
 #include <fcntl.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
 typedef struct s_list
 {
--- src/main.c
+++ src/main.c
@@ -40,59 +40,64 @@ char *which(char *cmd, char **envp)
 		out = mkpath(path[i], cmd);
 		if (open(out, O_RDONLY) != -1)
 		{
-			free_arr(path);
+			// free_arr(path);
 			return (out);
 		}
 	}
 	return (NULL);
 }
 
-void strappend(char **str1, char *str2)
+int exec(char *cmd, char **envp)
 {
-	int i;
-	int j;
-	char *new;
+	char **args;
 
-	new = malloc(strlen(*str1) + strlen(str2) + 1);
-	if (!new)
-		return ;
-	i = -1;
-	j = 0;
-	while ((*str1)[++i])
-		new[j++] = (*str1)[i];
-	i = -1;
-	while (str2[++i])
-		new[j++] = str2[i];
+	args = ft_split(cmd, ' ');
+	args[0] = which(args[0], envp);
+	if (execve(args[0], args, envp) == -1)
+		return (-1);
+	return (0);
 }
 
-char *get_input(char *file)
+int pipex(char *cmd, char **envp)
 {
-	char *out;
-	int fd;
-	size_t size;
-	ssize_t r;
-	char buff[1024];
+	int fdp[2];
+	pid_t pid;
 
-	fd = open(file, O_RDONLY);
-	if (fd < 0)
-		return (NULL);
-	size = 0;
-	while (r = read(fd, buff, 1024))
-		size += r;
-	close(fd);
-	fd = open(file, O_RDONLY);
-	out = malloc(size + 1);
-	size = 0;
-	while (1)
+	if (pipe(fdp) == -1)
+		return (-1);
+	pid = fork();
+	if (pid == -1)
+		return (-1);
+	if (!pid)
 	{
-		r = -1;
-		if (read(fd, buff, 1024))
-			break ;
-		while ()
+		close(fdp[0]);
+		dup2(fdp[1], STDOUT_FILENO);
+		exec(cmd, envp);
 	}
+	else
+	{
+		wait(NULL);
+		close(fdp[1]);
+		dup2(fdp[0], STDIN_FILENO);
+	}
+	return (0);
 }
 
 int main(int ac, char **av, char **envp)
 {
-	char *input;
+	int i;
+	int fdin;
+	int fdout;
+
+	if (ac < 5)
+		return (1);
+	fdin = open(av[1], O_RDONLY);
+	fdout = open(av[ac - 1], O_WRONLY | O_CREAT | O_TRUNC, 0777);
+	dup2(fdin, STDIN_FILENO);
+	i = 1;
+	while (++i < ac - 2)
+		pipex(av[i], envp);
+	dup2(fdout, STDOUT_FILENO);
+	exec(av[ac - 2], envp);
+	return (0);
 }
